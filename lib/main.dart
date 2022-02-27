afb7a3bdab8497d27b91a50564bf20d2f6a95fb3
Lucas Furtado <lucas.furtado@secondmind.com.br>
2022-02-27 15:59:10 -0300

    tela de crianção de tranferencia criada

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,96 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: ListaTranferencia(),
+  runApp(ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando tranferencia'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => null,
+            child: Text('Botão'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ListaTranferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Tranferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTranferencia(Transferencia(100.00, 1000)),
+          ItemTranferencia(Transferencia(200.00, 2000)),
+          ItemTranferencia(Transferencia(300.00, 3000)),
+          ItemTranferencia(Transferencia(400.00, 4000)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => null,
       ),
-    ),
-  ));
-}
-
-class ListaTranferencia extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Column(
-      children: <Widget> [
-        ItemTranferencia(Transferencia(100.00,1000)),
-        ItemTranferencia(Transferencia(200.00,2000)),
-        ItemTranferencia(Transferencia(300.00,3000)),
-        ItemTranferencia(Transferencia(400.00,4000)),
-      ],
     );
   }
 }
 
-class ItemTranferencia extends StatelessWidget{
-
+class ItemTranferencia extends StatelessWidget {
   final Transferencia _tranferencia;
 
   ItemTranferencia(this._tranferencia);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
