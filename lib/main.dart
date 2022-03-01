dd45f1611ef0de86f7c86dde81dceb0856a46f0d
Lucas Furtado <lucas.furtado@secondmind.com.br>
2022-03-01 12:34:22 -0300

    colors

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,17 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+          secondary: Colors.blueAccent[700],
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary
+        ),
+      ),
       home: Scaffold(
         body: ListaTranferencia(),
       ),
